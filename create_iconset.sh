e2350bb9cfbf9b9853a9fc2834a93b9ad186e997
Patrick Jenkins <me@patjenk.com>
2025-06-10 14:13:45 -0400

    Adding icon for creating a MacOS app.

---
+++ create_iconset.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ensure icon.png exists
+if [ ! -f icon.png ]; then
+  echo "Error: icon.png not found!"
+  exit 1
+fi
+
+# Create iconset folder
+mkdir -p MyIcon.iconset
+
+# Create PNGs for all icon sizes
+sips -z 16 16     icon.png --out MyIcon.iconset/icon_16x16.png
+sips -z 32 32     icon.png --out MyIcon.iconset/icon_16x16@2x.png
+sips -z 32 32     icon.png --out MyIcon.iconset/icon_32x32.png
+sips -z 64 64     icon.png --out MyIcon.iconset/icon_32x32@2x.png
+sips -z 128 128   icon.png --out MyIcon.iconset/icon_128x128.png
+sips -z 256 256   icon.png --out MyIcon.iconset/icon_128x128@2x.png
+sips -z 256 256   icon.png --out MyIcon.iconset/icon_256x256.png
+sips -z 512 512   icon.png --out MyIcon.iconset/icon_256x256@2x.png
+sips -z 512 512   icon.png --out MyIcon.iconset/icon_512x512.png
+sips -z 1024 1024 icon.png --out MyIcon.iconset/icon_512x512@2x.png
+
+echo "Icon PNGs created in MyIcon.iconset/"
+
